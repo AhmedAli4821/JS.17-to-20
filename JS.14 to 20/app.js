31f6a13b7055514cfe632e8c23bfd3043e41681e
Ahmed Ali <121734255+AhmedAli4821@users.noreply.github.com>
2023-02-21 12:26:39 -0800

    JS.17 to 20

---
+++ JS.14 to 20/app.js
@@ -0,0 +1,307 @@
+// var names = ["Ghouse","Ahmed","Ali","Raza"]
+
+// names[0] = "Raza"
+// names[3] = "Gouse"
+// console.log(names)
+
+// var names = ["Ghouse","Ahmed","Ali","Raza"]
+// names [0]=[names[3],names[3]=names[0]][0]
+// console.log(names)
+
+
+// document.write("<h1>Chapter 14 To 16</h1>")
+// document.write("<h3>Question No # 1</h3>")
+
+// var names = ["Gouse"];
+
+// names[1] = "Ahmed"
+// names[2] = "Ali"
+// names[5] = "Raza"
+// console.log(names)
+
+
+// document.write("<h3>Question No # 2</h3>")
+// var object = []
+// console.log(object)
+
+
+
+// document.write("<h3>Question No # 3</h3>")
+
+// var a = ["Ghouse","Ahmed","Ali","Raza"];
+// document.write(a);
+
+
+// document.write("<h3>Question No # 4</h3>")
+
+// var b = [11,30,12,63];
+// document.write(b);
+
+
+// document.write("<h3>Question No # 7</h3>")
+
+
+// var qualifications = ["SSC", "HSC", "BCS","Bs","Bcome","Ms","M Phil","PHD"]
+// document.write("<h3>Qualifications</h3>")
+// document.write(qualifications[0])
+// document.write("<br>")
+// document.write(qualifications[1])
+// document.write("<br>")
+// document.write(qualifications[2])
+// document.write("<br>")
+// document.write(qualifications[3])
+// document.write("<br>")
+// document.write(qualifications[4])
+// document.write("<br>")
+// document.write(qualifications[5])
+// document.write("<br>")
+// document.write(qualifications[6])
+// document.write("<br>")
+// document.write(qualifications[7])
+// document.write("<br>")
+
+// document.write("<h3>Question No # 8</h3>")
+
+// var students = ["Ahmed","Ali","Sajjad"]
+// var marks = [480,465,460]
+// document.write("Score of" +" "+students[0]+" "+ "is" 
+// +" "+marks[0]+" "+"Whose Percentage is"+" "+ marks[0]*100 / 500)
+// document.write("<br>")
+
+// document.write("Score of" +" "+ students[1] +" "+ "is" 
+// +" "+ marks[1] +" "+ "Whose Percentage is" +" "+ marks[1]*100/500)
+
+// document.write("<br>")
+// document.write("Score of" + " " +students[2] + " " +"is" 
+// +" "+ marks[2] +" "+ "Whose Percentage is"+" "+ marks[1]*100/500)
+// document.write("<br>")
+
+// document.write("<h3>Question No # 9 A</h3>")
+
+// var colors = ["red ", "yellow ", "green "]
+// document.write(colors)
+// document.write("<br>")
+// var color = prompt("Enter color")
+// document.write(color + " ," + colors)
+
+// document.write("<h3>Question No # 9 B</h3>")
+
+// var colors = ["red ", "yellow ", "green "]
+// document.write(colors)
+// document.write("<br>")
+// var color = prompt("Enter color")
+// document.write(colors + " ," + color)
+
+// document.write("<h3>Question No # 9 C</h3>")
+
+// var colors = ["red ", "yellow ", "green "]
+// colors.unshift("Pink","Bilow")
+
+// document.write(colors)
+
+
+// document.write("<h3>Question No # 9 D</h3>")
+// var colors = ["red ", "yellow ", "green "]
+// colors.shift()
+// document.write(colors)
+
+// document.write("<h3>Question No # 9 E</h3>")
+
+// var colors = ["red ", "yellow ", "green ","bilow"]
+//  colors.pop()
+//  document.write(colors)
+
+
+//  document.write("<h3>Question No # 9 F</h3>")
+
+//  var colors = ["Red ", "Yellow ", "Green ","Bilow"]
+//  colors.splice(1,0,"Pink")
+//  document.write(colors)
+
+
+//  document.write("<h3>Question No # 9 G</h3>")
+
+//  var colors = ["Red ", "Yellow ", "Green ","Bilow","Pink"]
+
+// colors.splice(3,1)
+// document.write(colors)
+
+// document.write("<h3>Question No # 10 </h3>")
+// var scores = ["320","230","480","120"]
+// document.write("Score of Students :"+" "+scores)
+// document.write("<br>")
+// scores.sort()
+// document.write("Ordered Score of Students"+" "+scores)
+ 
+// document.write("<h3>Question No # 11 </h3>")
+
+// var cities = ["Karachi","Lahore","islamabad","Quetta","Peshawar"]
+// document.write("Cities List :<br>")
+// document.write(cities)
+// document.write("<br>")
+// var copy = cities.slice(2,4)
+// document.write("Selected Cities List :<br>")
+// document.write(copy)
+
+// document.write("<h3>Question No # 12 </h3>")
+
+// var arr = ["This","is","my","cat"]
+// document.write("Array:<br>")
+// document.write(arr.join())
+// document.write("<br>")
+// document.write("String:")
+// document.write("<br>")
+// document.write(arr.join(" "))
+
+// document.write("<h3>Question No # 13 </h3>")
+
+// var order = ["Keyboard","Mouse","Printer","Monitore"]
+// document.write("Devices:<br>"+ order + "<br>")
+// document.write("Out:<br>" + order[0] + "<br>")
+// document.write("Out:<br>" + order[1] + "<br>")
+// document.write("Out:<br>" + order[2] + "<br>")
+// document.write("Out:<br>" + order[3] + "<br>")
+
+
+// document.write("<h3>Question No # 14 </h3>")
+
+// var order1 = ["Keyboard","Mouse","Printer","Monitore"]
+
+// document.write("Devices:<br>"+ order1 + "<br>")
+// document.write("Out:<br>" + order1[3] + "<br>")
+// document.write("Out:<br>" + order1[2] + "<br>")
+// document.write("Out:<br>" + order1[1] + "<br>")
+// document.write("Out:<br>" + order1[0] + "<br>")
+
+
+// document.write("<h3>Question No # 15 </h3>")
+// var mobile = ["Apple","Samsung","Motrola","Huawei","Nokia","Sony","Haier"]
+// document.write(
+//    "<select class=mobile>" + 
+//    "<option>apple<option>" +
+//    "<option>Samsung<option>" +
+//    "<option>Motorola<option>" +
+//    "<option>Huawei<option>" +
+//    "<option>Nokia<option>" +
+//    "<option>Sony<option>" +
+//    "<option>Haier<option>" +
+
+//    "</select>"
+//     )
+
+
+//     document.write("<h1>Chapter 17 To 20</h1>")
+var num = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+document.write(num[0]+"<br>")
+document.write(num[1]+"<br>")
+document.write(num[2])
+//  document.write("<h3>Question No # 2</h3>")
+//  var num1=[0,1,2,3];
+//  var num2=[1,0,1,2];
+//  var num3=[2,1,0,1];
+//  document.write(num1+"<br>"+num2+"<br>"+num3);
+
+
+//     document.write("<h3>Question No # 3</h3>")
+
+// for (var i = 1; i <= 10; i++){
+//      document.write(i + "<br/>")   
+// }
+
+// document.write("<h3>Question No # 4</h3>")
+
+// for (var i = 1; i <= 10; i++){
+//     document.write("2 ×" + i +"="+ 2 * i + "<br/>")
+// }
+
+
+document.write("<h3>Question No # 5</h3>")
+var arr = ["apple","banana","mango","Orange"]
+
+for (var i = 0; i < arr.length; i++)
+document.write("<h3>" + arr[i] + "<h3>")
+
+// document.write("<h3>"+ arr[0] +"</h3>")
+// document.write("<h3>"+ arr[1] +"</h3>")
+// document.write("<h3>"+ arr[2] +"</h3>")
+// document.write("<h3>"+ arr[3] +"</h3>")
+
+document.write("<h3>"+"Element at index 0 is"+" "+ arr[0] +"</h3>" )
+document.write("<h3>"+"Element at index 0 is"+" "+ arr[1] +"</h3>" )
+document.write("<h3>"+"Element at index 0 is"+" "+ arr[2] +"</h3>" )
+document.write("<h3>"+"Element at index 0 is"+" "+ arr[3] +"</h3>" )
+
+
+
+
+
+    // document.write("<h3>Question No # 7</h3>")
+
+    //  var backry = ["cake", "apple pie", "cookie", "chips", "patties",]
+    //  var costomer =prompt("Welcome To ABC Backery.What Do You Want To Order Sir/Ma'am?")
+    //  var flag = "nahi"
+    //  for(var i = 0; i < backry.length; i++){
+    //     if (costomer == backry[i]){
+    //         flag = "han"
+    //        break;
+    //      }
+    //  }
+    // if (flag == "han") {
+    //     alert("Available at index 2 in our Backr")
+    // }else {
+    //     alert("We are sorry is not Available in our Backr")
+    // }
+
+    // document.write("<h3>Question No # 6 A</h3>")
+
+    // for(var i = 1; i<= 10; i++)
+    // document.write(i+",")
+    
+    
+//     document.write("<h3>Question No # 6 B</h3>")
+  
+//    var num = [1,2,3,4,5,6,7,8,,9,10]
+//    num.reverse()
+//    document.write(num)
+
+//    document.write("<h3>Question No # 6 C Even</h3>")
+//    for(a = 0; a<= 20; a++){
+//     if(a % 2 == 0){
+//         document.write(a+",")
+//     }
+//    }
+
+//    document.write("<h3>Question No # 6 D odd</h3>")
+// for(b = 1;  b<= 19; b++){
+//     if(b % 2 != 0){
+//         document.write(b+",")
+//     }
+// }
+
+
+// document.write("<h3>Question No # 6 E odd</h3>")
+//  var c =["2k","4k","6k","8k","10k","12k","14k","16k","18k","20k"]
+//  for(i = 0; i<= c.length; i++){
+//  document.write(c[i]+",")
+//  console.log(c[i])
+// }
+//     // document.write("<h3>Question No # 7</h3>")
+    // if (a[0] == costomer){
+    //     alert ("cake is Available at index 2 in our Backr")
+    // }else if (a[1] == costomer){
+    //     alert ("apple pie is Available at index 2 in our Backr")
+    // }else if (a[2] == costomer){
+    //     alert ("cookie is Available at index 2 in our Backr")
+    // }else if (a[3] == costomer){
+    //     alert ("chips is Available at index 2 in our Backr")
+    // }else if (a[4] == costomer){
+    //     alert ("patties is Available at index 2 in our Backr")
+    // }else if (a[5] == costomer){
+    //     alert ("We are sorry.pastry is not Available in our Backr")
+    // }else {
+    //     alert (" is nat Available in our Backr")
+    // }
+ 
+
+    
+    document.write("<h3>Question No # 7</h3>")
